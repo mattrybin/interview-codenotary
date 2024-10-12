91452f78dd10f20eb7e5088a51a93c9e63b06761
Matt Rybin <contact@mattrybin.com>
2024-10-12 04:20:57 +0200

    feat: rename and the text fit task

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@remix-run/react"
 import {
-  Bell,
   CircleUser,
+  HandCoins,
   Home,
   LineChart,
   Menu,
@@ -25,12 +25,6 @@ import {
 } from "../components/ui/dropdown-menu"
 import { Input } from "../components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet"
-// import { LoaderFunction } from "@remix-run/node"
-
-// // export const loader: LoaderFunction = async () => {
-// //   // You can load data here if needed
-// //   return null // or return { } if you prefer
-// // }
 
 export const description =
   "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
@@ -39,81 +33,31 @@ export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link
-              to="/"
-              className="flex items-center gap-2 font-semibold"
-            >
-              <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
-            </Link>
-            <Button
-              variant="outline"
-              size="icon"
-              className="ml-auto h-8 w-8"
-            >
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                to="/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <Home className="h-4 w-4" />
-                Dashboard
-              </Link>
-              <Link
-                to="/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <ShoppingCart className="h-4 w-4" />
-                Orders
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                  6
-                </Badge>
-              </Link>
-              <Link
-                to="/"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-              >
-                <Package className="h-4 w-4" />
-                Products{" "}
-              </Link>
-              <Link
-                to="/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <Users className="h-4 w-4" />
-                Customers
-              </Link>
-              <Link
-                to="/"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <LineChart className="h-4 w-4" />
-                Analytics
-              </Link>
-            </nav>
-          </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
+        <div className="flex max-h-screen flex-col gap-2">
+          <Branding />
+          <Navigation />
+          <div className="p-4">
+            <Card>
               <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support team.
-                </CardDescription>
+                <CardTitle>Get Started</CardTitle>
+                <CardDescription>Create a new account or make a transaction.</CardDescription>
               </CardHeader>
               <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button
-                  size="sm"
-                  className="w-full"
-                >
-                  Upgrade
-                </Button>
+                <div className="flex flex-col gap-2">
+                  <Button
+                    size="sm"
+                    className="w-full"
+                  >
+                    Create Account
+                  </Button>
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="w-full"
+                  >
+                    Create Transaction
+                  </Button>
+                </div>
               </CardContent>
             </Card>
           </div>
@@ -253,4 +197,61 @@ export default function Dashboard() {
       </div>
     </div>
   )
+
+  function Navigation() {
+    return (
+      <div className="flex-1">
+        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+          <Link
+            to="/"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <Home className="h-4 w-4" />
+            Dashboard
+          </Link>
+          <Link
+            to="/"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <HandCoins className="h-4 w-4" />
+            Transactions
+          </Link>
+          <Link
+            to="/"
+            className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+          >
+            <Package2 className="h-4 w-4" />
+            Accounts
+          </Link>
+          <Link
+            to="/"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <LineChart className="h-4 w-4" />
+            <span>Analytics</span>
+            <Badge
+              variant="secondary"
+              className="ml-auto"
+            >
+              Coming Soon
+            </Badge>
+          </Link>
+        </nav>
+      </div>
+    )
+  }
+
+  function Branding() {
+    return (
+      <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <Link
+          to="/"
+          className="flex items-center gap-2 font-semibold"
+        >
+          <HandCoins className="h-6 w-6" />
+          <span className="">ImmuBank</span>
+        </Link>
+      </div>
+    )
+  }
 }
