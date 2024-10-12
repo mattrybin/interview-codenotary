cb8ba11b9070a2e3561b432283d549309b54d29b
Matt Rybin <contact@mattrybin.com>
2024-10-12 05:50:48 +0200

    feat: add accounts and transactions pages

--- frontend/app/routes/transactions.tsx
+++ frontend/app/routes/transactions.tsx
@@ -1,12 +1,12 @@
-export default function Dashboard() {
+export default function Transactions() {
   return (
     <>
       <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+        <h1 className="text-lg font-semibold md:text-2xl">Transactions</h1>
       </div>
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
         <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">Welcome to your dashboard</h3>
+          <h3 className="text-2xl font-bold tracking-tight">You have no transactions</h3>
         </div>
       </div>
     </>
