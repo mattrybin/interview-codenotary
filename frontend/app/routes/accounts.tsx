fd29a8361565962613d3bdcfc7ca272e1027c594
Matt Rybin <contact@mattrybin.com>
2024-10-16 07:19:51 +0200

    feat: add account id to single account page

--- frontend/app/routes/accounts.tsx
+++ frontend/app/routes/accounts.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from "@remix-run/react"
+import { Outlet, useParams } from "@remix-run/react"
 
 export default function AccountsLayout() {
+  const params = useParams()
+  const title = params.id ? `Account: ${params.id}` : "Accounts"
+
   return (
     <>
-      <h1 className="text-lg font-semibold md:text-2xl mb-4">Accounts</h1>
+      <h1 className="text-lg font-semibold md:text-2xl mb-4">{title}</h1>
       <Outlet />
     </>
   )
