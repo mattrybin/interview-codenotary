a4d0c825bfb79485b70afa479142aab234a81cf2
Matt Rybin <contact@mattrybin.com>
2024-10-16 09:57:03 +0200

    fix: test inside Navigation

--- frontend/app/components/layout/Navigation.cy.tsx
+++ frontend/app/components/layout/Navigation.cy.tsx
@@ -7,10 +7,6 @@ describe("<Navigation />", () => {
 
   it("renders all navigation links", () => {
     cy.get('[data-testid="nav-link-dashboard"]').should("be.visible").and("contain", "Dashboard")
-    cy.get('[data-testid="nav-link-transactions"]')
-      .should("be.visible")
-      .and("contain", "Transactions")
     cy.get('[data-testid="nav-link-accounts"]').should("be.visible").and("contain", "Accounts")
-    cy.get('[data-testid="nav-link-analytics"]').should("be.visible").and("contain", "Analytics")
   })
 })
